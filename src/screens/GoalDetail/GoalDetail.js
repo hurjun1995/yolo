1608f93668586b33cf5443d6c0f704f9869751f6
hurjun1995 <hurjun1995@gmail.com>
2018-10-10 21:12:20 -0700

    Navigation to Goal Detail screen done

--- src/screens/GoalDetail/GoalDetail.js
+++ src/screens/GoalDetail/GoalDetail.js
@@ -1,6 +1,26 @@
 // @flow
 import React from 'react'
 import { View, Text } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
+
+const list = [
+  {
+    name: 'Name',
+    icon: 'lightbulb-outline',
+  },
+  {
+    name: 'Start Date',
+    icon: 'calendar-range',
+  },
+  {
+    name: 'When to Log',
+    icon: 'lead-pencil',
+  },
+  {
+    name: 'Reminder',
+    icon: 'bell-ring-outline',
+  },
+]
 
 type Props = {}
 class GoalDetailScreen extends React.Component<Props> {
@@ -8,9 +28,17 @@ class GoalDetailScreen extends React.Component<Props> {
 
   render() {
     return (
-      <View>
-        <Text>GoalDetailScreen</Text>
-      </View>
+      <List containerStyle={{ marginBottom: 20 }}>
+        {list.map(l => (
+          <ListItem
+            textInput
+            hideChevron
+            leftIcon={{ name: l.icon, type: 'material-community' }}
+            key={l.name}
+            title={l.name}
+          />
+        ))}
+      </List>
     )
   }
 }
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,12 +1,10 @@
 import { Navigation } from 'react-native-navigation'
 import Icon from 'react-native-vector-icons/Ionicons'
 
-import { platformSpecific } from '../../utility/util'
-
 const startMainTabs = () => {
   Promise.all([
-    Icon.getImageSource(platformSpecific('md-list-box', 'ios-list-box'), 30),
-    Icon.getImageSource(platformSpecific('md-settings', 'ios-settings'), 30),
+    Icon.getImageSource('ios-list-box', 30),
+    Icon.getImageSource('ios-settings', 30),
   ]).then((sources) => {
     Navigation.setRoot({
       root: {
