e5907139c5a0b6fa6130acc4649cc8f23c9fc3cd
hurjun1995 <hurjun1995@gmail.com>
2019-01-19 18:12:30 -0800

    GoalName, StartDate,WhenToLog,Reminder added to GoalDetail screen; still need to work on DatePicker's behaviour

--- src/screens/GoalDetail/GoalDetail.js
+++ src/screens/GoalDetail/GoalDetail.js
@@ -2,43 +2,99 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { List, ListItem } from 'react-native-elements'
-
-const list = [
-  {
-    name: 'Name',
-    icon: 'lightbulb-outline',
-  },
-  {
-    name: 'Start Date',
-    icon: 'calendar-range',
-  },
-  {
-    name: 'When to Log',
-    icon: 'lead-pencil',
-  },
-  {
-    name: 'Reminder',
-    icon: 'bell-ring-outline',
-  },
-]
+import DatePicker from 'react-native-datepicker'
 
 type Props = {}
-class GoalDetailScreen extends React.Component<Props> {
-  state = {}
+type State = {
+  goalName: string,
+  startDate: string,
+  logTime: string,
+  ReminderTime: string,
+}
+class GoalDetailScreen extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      goalName: 'sdvads',
+      startDate: '2001-01-20',
+      logTime: '',
+      ReminderTime: '',
+    }
+  }
+
+  onGoalNamePress = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      goalName: `${prevState.goalName}a`,
+    }))
+    // TODO:
+  }
+
+  onStartDatePress = () => {
+    // TODO:
+  }
+
+  onLogTimePress = () => {
+    // TODO:
+  }
+
+  onReminderTimePress = () => {
+    // TODO:
+  }
+
+  onStartDateChange = (dateStr) => {
+    this.setState({ startDate: dateStr })
+  }
 
   render() {
     return (
-      <List containerStyle={{ marginBottom: 20 }}>
-        {list.map(l => (
+      <View>
+        <List containerStyle={{ marginBottom: 20 }}>
           <ListItem
-            textInput
-            hideChevron
-            leftIcon={{ name: l.icon, type: 'material-community' }}
-            key={l.name}
-            title={l.name}
+            title="Goal Name"
+            rightTitle={this.state.goalName}
+            onPressRightIcon={this.onGoalNamePress}
           />
-        ))}
-      </List>
+          <ListItem
+            title="Start Date"
+            rightIcon={(
+              <DatePicker
+                date={this.state.startDate}
+                onDateChange={this.onStartDateChange}
+                customStyles={{ dateInput: { borderWidth: 0 } }}
+                showIcon={false}
+                cancelBtnText="Cancel"
+                confirmBtnText="Confirm"
+              />
+)}
+            onPressRightIcon={this.onStartDatePress}
+          />
+          <ListItem
+            title="When to Log"
+            rightIcon={(
+              <DatePicker
+                showIcon={false}
+                mode="time"
+                customStyles={{ dateInput: { borderWidth: 0 } }}
+                cancelBtnText="Cancel"
+                confirmBtnText="Confirm"
+              />
+)}
+            onPressRightIcon={this.onLogTimePress}
+          />
+          <ListItem
+            title="Reminder"
+            rightIcon={(
+              <DatePicker
+                showIcon={false}
+                mode="time"
+                customStyles={{ dateInput: { borderWidth: 0 } }}
+              />
+)}
+            onPressRightIcon={this.onReminderTimePress}
+          />
+        </List>
+      </View>
     )
   }
 }
