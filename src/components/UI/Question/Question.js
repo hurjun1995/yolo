8fad4951e3b8da44ac3e0f8b9c28e0fc4491dbc3
joon.hur <hurjun1995@gmail.com>
2019-01-25 17:07:29 -0800

    GoalSurvey screen and Question component placeholder added

---
+++ src/components/UI/Question/Question.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Question = props => {
+    return (
+        <View>
+            <Text>Question placeholder</Text>
+        </View>
+    )
+}
+
+export default Question
---
+++ src/screens/GoalSurvey/GoalSurvey.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+import Question from '../../component/UI/Question/Question'
+
+class GoalSurvey extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Goal Survey Screen placeholder!</Text>
+            </View>
+        )
+    }
+}
+
+export default GoalSurvey
