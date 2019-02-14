125dc45d85b7b360cba26980ef353e6690321ff6
hurjun1995 <hurjun1995@gmail.com>
2019-02-13 21:37:42 -0800

    added generateScaleButtons to ScaleAnswer

--- src/components/UI/Answer/ScaleAnswer.js
+++ src/components/UI/Answer/ScaleAnswer.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { Badge } from 'react-native-elements'
+
+import styles from './styles'
+
+const selectedBadge = '1'
+
+const generateScaleButtons = (numOfScale) => {
+  const retArr = []
+  for (let i = 1; i < numOfScale; i++) {
+    console.log(i)
+    retArr.push(
+      <Badge
+        key={i}
+        value={i}
+        containerStyle={
+          selectedBadge === i.toString()
+            ? [styles.badgeBase, styles.selectedBadge]
+            : styles.badgeBase
+        }
+      />,
+    )
+  }
+  return retArr
+}
 
 const ScaleAnswer = (props) => {
-  const ScaleAnswerPlaceholder = 'ScaleAnswerPlaceholder'
-  return (
-    <View>
-      <Text>{ScaleAnswerPlaceholder}</Text>
-    </View>
-  )
+  console.log('hi')
+  return <View style={{ flexDirection: 'row' }}>{generateScaleButtons(8)}</View>
 }
 
 export default ScaleAnswer
---
+++ src/components/UI/Answer/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  badgeBase: {
+    margin: 3,
+    backgroundColor: '#344164',
+  },
+  selectedBadge: {
+    backgroundColor: '#f8c74d',
+  },
+})
+
+export default styles
